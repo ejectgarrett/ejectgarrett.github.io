038b9466e3019e098a1a0729b0d71daf5fabedfa
Elias Seidman <eliasseidman@gmail.com>
2026-02-21 13:49:37 -0500

    done

--- thegame.cpp
+++ thegame.cpp
@@ -10,45 +10,39 @@ int main () {
     const int SCREEN_HEIGHT = 600;
     cout << "HEIGHT: " << SCREEN_HEIGHT  << "WIDTH: " << SCREEN_WIDTH << endl;
     int targetFPS = 60;
-    int ball_x = 100;
-    int ball_y = 100;
-    int ball_speed_x = 5;
-    int ball_speed_y = 5;
-    int ball_radius = 15;
+    int dvd_x = 100;
+    int dvd_y = 100;
+    int dvd_speed_x = 5;
+    int dvd_speed_y = 5;
+    int dvd_radius = 200;
     cout << "Hello World" << endl;
 
     InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "My first RAYLIB program!");
     SetTargetFPS(60);
     int color = 0;
     Color colors[6] = {RED, ORANGE, YELLOW, GREEN, BLUE, MAGENTA};
-    while (!WindowShouldClose()){
+    Texture2D dvd = LoadTexture("dvd.png");
+    while (!WindowShouldClose()) {
    
-        ball_x += ball_speed_x;
-        ball_y += ball_speed_y;
+        dvd_x += dvd_speed_x;
+        dvd_y += dvd_speed_y;
 
-        if(ball_x + ball_radius >= SCREEN_WIDTH || ball_x - ball_radius <= 0)
+        if (dvd_x + dvd_radius >= SCREEN_WIDTH || dvd_x - dvd_radius <= 0)
         {
-            cout << "X HIT!" << endl;
-            ball_speed_x *= -1;
+            dvd_speed_x *= -1;
         }
 
-        if(ball_y + ball_radius >= SCREEN_HEIGHT || ball_y - ball_radius <= 0)
+        if (dvd_y + dvd_radius >= SCREEN_HEIGHT || dvd_y - dvd_radius <= 0)
         {
-            cout << "Y HIT!" << endl;
-            ball_speed_y *= -1;
+            dvd_speed_y *= -1;
         }
+
         
         BeginDrawing();
         ClearBackground(BLACK);
-
-        if (IsKeyPressed(KEY_UP))
-            color = (color + 1) % 6;
-        if (IsKeyPressed(KEY_DOWN))
-            color = (color - 1) % 6;
-
-        DrawCircle(ball_x,ball_y,ball_radius, colors[color]);
+        DrawTexture(dvd, dvd_x,dvd_y, WHITE);
         EndDrawing();
     }
-
+    UnloadTexture(dvd);
     CloseWindow();
 }
